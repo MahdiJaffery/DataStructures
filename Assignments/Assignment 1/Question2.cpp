00668116ad1932e65a414475c1a0201d2b58e53e
Mahdi Jaffery <mahdijaffri5@gmail.com>
2024-10-25 02:40:38 +0500

    Rotate List

--- Assignments/Assignment 1/Question2.cpp
+++ Assignments/Assignment 1/Question2.cpp
@@ -8,6 +8,18 @@ int main()
     for (int i = 1; i <= 6; i++)
         mySet.insertData(i * 10);
 
+    if (mySet.rotateList(8))
+        cout << "List rotated\n";
+    else
+        cout << "Failed to rotate list\n";
+
+    mySet.print();
+
+    if (mySet.rotateList(-3))
+        cout << "List rotated\n";
+    else
+        cout << "Failed to rotate list\n";
+
     mySet.print();
 
     return 0;
